cbcd65225a34b74b2f18be4e21c23f409ad405aa
kolombet <anonrab@ya.ru>
2018-02-21 22:30:18 +0300

    closest bar

--- bars.py
+++ bars.py
@@ -1,6 +1,7 @@
 import json
 from pathlib import Path
 import codecs
+import math
 
 
 def load_data(filepath):
@@ -8,7 +9,6 @@ def load_data(filepath):
     if not is_file:
         print("file not found")
         return -1
-
     file = codecs.open(filepath, 'r', 'utf-8')
     data = file.read()
     file.close()
@@ -25,9 +25,7 @@ def get_biggest_bar(data):
         if count > max_value:
             max_value = count
             max_bar = bar
-
     return max_bar["properties"]["Attributes"]["Name"]
-    pass
 
 
 def get_smallest_bar(data):
@@ -40,19 +38,34 @@ def get_smallest_bar(data):
         if count != 0 and count < min_value:
             min_value = count
             min_bar = bar
-
     return min_bar["properties"]["Attributes"]["Name"]
-    pass
 
 
 def get_closest_bar(data, longitude, latitude):
-    pass
+    bars = data["features"]
+    coordinates = bars[0]["geometry"]["coordinates"]
+    min_distance = math.sqrt((coordinates[0] - latitude) ** 2 + (coordinates[1] - longitude) ** 2)
+    min_bar = bars[0]
+    for bar in bars:
+        coordinates = bar["geometry"]["coordinates"]
+        distance = math.sqrt((coordinates[0] - latitude) ** 2 + (coordinates[1] - longitude) ** 2)
+        if distance < min_distance:
+            min_distance = distance
+            min_bar = bar
+    return min_bar["properties"]["Attributes"]["Name"]
+
+
+def calculate_distance(from_x, from_y, to_x, to_y):
+    return math.sqrt((from_x - to_x) ** 2 + (from_y - to_y) ** 2)
 
 
 if __name__ == '__main__':
     bar_data = load_data("data.json")
-    smallest = get_smallest_bar(bar_data)
-    print("smallest bar: " + smallest)
-    biggest = get_biggest_bar(bar_data)
-    print("biggest bar: " + biggest)
+    if bar_data:
+        smallest = get_smallest_bar(bar_data)
+        print("smallest bar: " + smallest)
+        biggest = get_biggest_bar(bar_data)
+        print("biggest bar: " + biggest)
+        closest = get_closest_bar(bar_data, 55.856233, 37.584603)
+        print("closest bar: " + closest)
     pass
